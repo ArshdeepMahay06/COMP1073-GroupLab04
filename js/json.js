a04095962faad336dd2504d101d13b7549c3900e
Vanshika verma <109541924+vanshikaverma2003@users.noreply.github.com>
2025-03-27 20:59:19 -0400

    Invoke the populateHeader function here, then build it below

--- js/json.js
+++ js/json.js
@@ -19,5 +19,7 @@ async function populate() {
   const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console
     console.log(iScream);
+  // STEP 9a: Invoke the populateHeader function here, then build it below
+  populateHeader(iScream);
 
 
